9a257b2fe883610639b874d3d8965e44cdc4e42a
Matheus <matheus.frndes@gmail.com>
2015-11-09 13:00:53 -0200

    Build variables

---
+++ app/src/main/java/io/msgapp/android/BuildVars.java
@@ -0,0 +1,9 @@
+package io.msgapp.android;
+
+/**
+ * Created by matheus on 11/8/15.
+ */
+public class BuildVars {
+    public static String API_BASE_URL = "http://IP:PORT/api/";
+    public static String LOG_TAG      = "MSG_LOG_DEBUG";
+}
