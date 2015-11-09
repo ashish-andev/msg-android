3abccfdbe24bb5838626f0dc8339524c4cb05137
Matheus <matheus.frndes@gmail.com>
2015-11-09 12:59:17 -0200

    Retrofit API interface

---
+++ app/src/main/java/io/msgapp/android/backend/Api.java
@@ -0,0 +1,14 @@
+package io.msgapp.android.backend;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Created by matheus on 11/8/15.
+ */
+public interface Api {
+
+    @GET("public/username/available")
+    Call<Boolean> usernameIsAvailable(@Query("username") String username);
+}
