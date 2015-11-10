e930838ea5c358ed44ca098c9e503efed9db437b
Matheus <matheus.frndes@gmail.com>
2015-11-10 20:27:57 -0200

    Update API interface (https://github.com/matheuss/msg-backend/commit/2124e32d9c36bd67c4007ce808c2e7b8be781ca0)

--- app/src/main/java/io/msgapp/android/backend/Api.java
+++ app/src/main/java/io/msgapp/android/backend/Api.java
@@ -9,9 +9,9 @@ import retrofit.http.Query;
  */
 public interface Api {
 
-    @GET("public/username/available")
+    @GET("users/username/available")
     Call<Boolean> usernameIsAvailable(@Query("username") String username);
 
-    @GET("public/email/available")
+    @GET("users/email/available")
     Call<Boolean> emailIsAvailable(@Query("email") String username);
 }
