1aa9913845a943cf3e6bcfa8e5d0f4466f35ed76
Matheus <matheus.frndes@gmail.com>
2015-11-11 17:29:11 -0200

    Receive result from Sign Up activity & ensure that views have unique ids

--- app/src/main/java/io/msgapp/android/SignInActivity.java
+++ app/src/main/java/io/msgapp/android/SignInActivity.java
@@ -6,10 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import butterknife.Bind;
 
 public class SignInActivity extends AppCompatActivity {
+    private static final int SIGN_UP_CODE = 666;
 
-    private static AppCompatActivity _this;
+    @Bind(R.id.et_username_sign_in)
+    protected EditText eTUsername;
+
+    @Bind(R.id.et_password_sign_in)
+    protected EditText eTPassword;
+
+    @Bind(R.id.progressBar_sign_in)
+    protected ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +30,6 @@ public class SignInActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.actionBar);
         setSupportActionBar(myToolbar);
-
-        _this = this;
-
     }
 
     @Override
@@ -33,14 +42,25 @@ public class SignInActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.actionSignUp:
-                startActivity(new Intent(this, SignUpActivity.class));
+                startActivityForResult(new Intent(this, SignUpActivity.class), SIGN_UP_CODE);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public static void signUpSucceeded() {
-        _this.finish();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SIGN_UP_CODE:
+                if (resultCode == RESULT_OK) {
+                    this.signIn(data.getStringExtra("username"), data.getStringExtra("password"));
+                }
+        }
+    }
+
+    private void signIn(String username, String password) {
+        // TODO
     }
 }
