3cc980edb5aebf953e12cec75ec2d16e204a6f93
Matheus <matheus.frndes@gmail.com>
2015-11-18 17:24:53 -0200

    Handle "msgapp.me" links & updated to enhanced chat preview

--- app/src/main/java/io/msgapp/android/MainActivity.java
+++ app/src/main/java/io/msgapp/android/MainActivity.java
@@ -1,15 +1,29 @@
 package io.msgapp.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import io.msgapp.android.model.ChatPreview;
+import io.msgapp.android.model.User;
+import io.socket.emitter.Emitter;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+import static io.msgapp.android.BuildVars.LOG_TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
         this.app = ((App) getApplication()).init();
 
         if (!this.app.signedIn()) {
-//            startActivity(new Intent(this, SignInActivity.class));
-//            finish();
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
         }
 
         super.onCreate(savedInstanceState);
@@ -36,20 +50,125 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        this.handleIntent();
+
         SocketManager.getInstance(app).connect();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-            }
-        });
+//            }
+//        });
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(new MainRecyclerViewAdapter());
+
+        List<ChatPreview> list = new ArrayList<>();
+        list.add(new ChatPreview("", "John", "Hey man!!", "20:34", ChatPreview.STATUS_SENT));
+        list.add(new ChatPreview("", "Lara", "Good night ^~^", "20:03", ChatPreview.STATUS_READ));
+        list.add(new ChatPreview("", "Frank", "I'll check that", "19:22", ChatPreview.STATUS_DELIVERED));
+        list.add(new ChatPreview("", "Bob", "Ok, send me an email", "14:46", ChatPreview.STATUS_READ));
+        list.add(new ChatPreview("", "Jack", "You too", "12:27", 0));
+
+        recyclerView.setAdapter(new MainRecyclerViewAdapter(list));
+        Log.d(LOG_TAG, "MainActivity onCreate");
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "MainActivity onStart");
+        this.handleIntent();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    private void handleIntent() {
+        Intent intent = this.getIntent();
+
+        if (intent.getAction().equals(Intent.ACTION_VIEW)) {
+            Uri uri = intent.getData();
+
+            if (Objects.equals(uri.getHost(), "msgapp.me")) {
+                String username = uri.getPathSegments().get(0);
+
+                app.getApi().getUserByUsername("Bearer " + app.getCurrentUser().getString("access_token", ""), username)
+                        .enqueue(new Callback<List<User>>() {
+                            @Override
+                            public void onResponse(Response<List<User>> response, Retrofit retrofit) {
+                                if (response.isSuccess()) {
+                                    User user = response.body().get(0);
+
+                                    SocketManager.getInstance(app).
+                                            addListener(SocketManager.EVENT_SERVER_CONVERSATION_CREATED,
+                                                    onConversationCreated);
+                                    SocketManager.getInstance(app)
+                                            .send(SocketManager.EVENT_CONVERSATION_CREATE,
+                                                    "{\"userId\": " + user.id + "}");
+
+                                } else {
+                                    try {
+                                        Log.d(LOG_TAG, response.errorBody().string());
+                                        app.somethingWentWrong();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                        app.somethingWentWrong();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Throwable t) {
+
+                            }
+                        });
+
+
+            }
+        }
+    }
+
+    private final Emitter.Listener onConversationCreated = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Log.d(LOG_TAG, "conversation created");
+            Log.d(LOG_TAG, args[0].toString());
+
+        }
+    };
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//
+//        return true;
+//    }
+//
+//    private void test() {
+//        try {
+//            JSONObject obj = new JSONObject();
+//            obj.put("to", "2");
+//            obj.put("text", "^~^");
+//            obj.put("type", "text");
+//            String now = String.valueOf(System.currentTimeMillis());
+//            String hash = app.getCurrentUser().getString("access_token", "");
+//            hash += now + "^~^";
+//            Log.d(LOG_TAG, hash);
+//            hash = Util.sha1(hash);
+//            Log.d(LOG_TAG, hash);
+//            obj.put("hash", hash);
+//            obj.put("when", now);
+//            SocketManager.getInstance(app).send("message", obj);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
