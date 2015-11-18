925ca53edf7d5da56b3610fa7399aeb8557bf340
Matheus <matheus.frndes@gmail.com>
2015-11-18 17:18:38 -0200

    POJO

---
+++ app/src/main/java/io/msgapp/android/model/ChatPreview.java
@@ -0,0 +1,27 @@
+package io.msgapp.android.model;
+
+/**
+ * Created by matheus on 11/16/15.
+ */
+public class ChatPreview {
+    public String avatarUrl;
+    public String userName;
+    public String messagePreview;
+    public String messageWhen;
+    public int status;
+
+    // TODO: move these constants to a more suitable class
+    public final static int STATUS_LOCAL = 1;
+    public final static int STATUS_SENT = 2;
+    public final static int STATUS_DELIVERED = 3;
+    public final static int STATUS_READ = 4;
+
+    public ChatPreview(String avatarUrl, String userName, String messagePreview, String messageWhen,
+                       int status) {
+        this.avatarUrl = avatarUrl;
+        this.userName  = userName;
+        this.messagePreview = messagePreview;
+        this.messageWhen = messageWhen;
+        this.status = status;
+    }
+}
